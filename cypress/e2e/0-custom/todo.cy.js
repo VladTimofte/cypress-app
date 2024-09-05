1ac1d1b5569affc88f4db8afca0d36b734bfda16
Constantin Stoica <161923684+Stoica-Constantin27@users.noreply.github.com>
2024-09-05 19:30:42 +0300

    init js

--- cypress/e2e/0-custom/todo.cy.js
+++ cypress/e2e/0-custom/todo.cy.js
@@ -1,16 +1,29 @@
-describe('Todo App', () => {
-    beforeEach(()=> {
-        cy.visit('http://localhost:4200/')
-    }) 
+describe("Todo App", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:4200/");
+  });
 
-    it('ar trebui sa adauge un todo nou si sa sa verifice lungimea, valoarea si continutul textului', () => {
-        cy.get('input[placeholder="Adaugă o sarcină nouă"]').type('Sa ma uit la meci maine');
-        cy.get('button.add-task').click()
+  it("ar trebui sa adauge un todo nou si sa sa verifice lungimea, valoarea si continutul textului", () => {
+    cy.get('input[placeholder="Adaugă o sarcină nouă"]').type(
+      "Sa ma uit la meci maine"
+    );
+    cy.get("button.add-task").click();
 
-        cy.get('ul li').should('have.length', 1)
+    cy.get("ul li").should("have.length", 1);
 
-        cy.get('input[placeholder="Adaugă o sarcină nouă"]').should('have.value', '')
+    cy.get('input[placeholder="Adaugă o sarcină nouă"]').should(
+      "have.value",
+      ""
+    );
 
-        cy.get('ul li span').should('contain.text', 'Sa ma uit la meci maine')
-    })
-})
+    cy.get("ul li span").should("contain.text", "Sa ma uit la meci maine");
+  });
+  it("Ar trebui sa marcheze un todo, ca fiind completat si sa verifice clasa si starea lui", () => {
+    cy.get('input[placeholder="Adaugă o sarcină nouă"]').type("Ana are mere");
+    cy.get("button.add-task").click();
+
+    cy.get('input[type="checkbox"]').check();
+
+    cy.get("span").should("have.class", "done");
+  });
+});
