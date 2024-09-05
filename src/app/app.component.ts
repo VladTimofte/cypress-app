76b8d36f28d439cb72e7fec3a4fc20f89ea8abc6
Vlad <vladtimofte999@gmail.com>
2024-09-05 17:16:04 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TodoComponent } from './todo/todo.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { JokeComponent } from './joke/joke.components';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodoComponent, LoginFormComponent, ProductListComponent, JokeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
